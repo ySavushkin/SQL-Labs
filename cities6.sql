cfbff3f9b030ee56aa058cf138d953ba48e8087a
ySavushkin <144417407+ySavushkin@users.noreply.github.com>
2023-10-25 01:16:51 +0200

    lab6

---
+++ cities6.sql
@@ -0,0 +1,67 @@
+1. 
+SELECT cities.name, regions.name
+FROM cities, regions
+WHERE population > 350000
+AND cities.region = regions.uuid
+2. 
+SELECT cities.name, regions.name
+FROM cities, regions
+WHERE cities.region = regions.uuid
+AND regions.name = "Nord"
+3.
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+
+DROP TABLE IF EXISTS `lines`;
+CREATE TABLE `lines` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `color` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `stations`;
+CREATE TABLE `stations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `line` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+INSERT INTO `lines`(`id`, `name`, `color`) VALUES
+(1, 'Холодногірсько-Заводська' , 'Червоний'),
+(2, 'Салтівська' , 'Синій'),
+(3, 'Олексіївська' , 'Зелений');
+
+INSERT INTO `stations`(`id`, `name`, `line`) VALUES
+(1, 'Холодна гора' , 'Холодногірсько-Заводська'),
+(2, 'Південний вокзал' , 'Холодногірсько-Заводська'),
+(3, 'Центральний ринок' , 'Холодногірсько-Заводська'),
+(4, 'Майдан Конституції' , 'Холодногірсько-Заводська'),
+(5, 'Проспект Гагаріна' , 'Холодногірсько-Заводська'),
+(6, 'Спортивна' , 'Холодногірсько-Заводська'),
+(7, 'Турбоатом' , 'Холодногірсько-Заводська'),
+(8, 'Дворець спорта' , 'Холодногірсько-Заводська'),
+(9, 'Армійська' , 'Холодногірсько-Заводська'),
+(10, 'Тракторний завод' , 'Холодногірсько-Заводська'),
+(11, 'Індустріальна' , 'Холодногірсько-Заводська'),
+(12, 'Історичний музей' , 'Салтівська'),
+(13, 'Університет' , 'Салтівська'),
+(14, 'Пушкінська' , 'Салтівська'),
+(15, 'Київська' , 'Салтівська'),
+(16, 'Академіка Барабашова' , 'Салтівська'),
+(17, 'Академіка Павлова' , 'Салтівська'),
+(18, 'Студентська' , 'Салтівська'),
+(19, 'Героїв Праці' , 'Салтівська'),
+(20, 'Метробудівників' , 'Олексіївська'),
+(21, 'Захисників України' , 'Олексіївська'),
+(22, 'Архітектора Бекетова' , 'Олексіївська'),
+(23, 'Держпром' , 'Олексіївська'),
+(24, 'Наукова' , 'Олексіївська'),
+(25, 'Ботанічний сад' , 'Олексіївська'),
+(26, '23 Серпня' , 'Олексіївська'),
+(27, 'Олексіївська' , 'Олексіївська'),
+(28, 'Перемога' , 'Олексіївська');
