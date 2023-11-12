9c34d02073169b6550de203aba93372e88a100f6
ySavushkin <144417407+ySavushkin@users.noreply.github.com>
2023-11-12 15:11:05 +0100

    lab8

---
+++ cities8.sql
@@ -0,0 +1,43 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS projects (
+    project_id INT PRIMARY KEY,
+    project_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS project_members (
+    project_id INT,
+    user_id INT,
+    PRIMARY KEY (project_id, user_id),
+    FOREIGN KEY (project_id) REFERENCES projects(project_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS tasks (
+    task_id INT PRIMARY KEY,
+    project_id INT,
+    author_id INT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES projects(project_id),
+    FOREIGN KEY (author_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS task_assignees (
+    task_id INT,
+    user_id INT,
+    PRIMARY KEY (task_id, user_id),
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS files (
+    file_id INT PRIMARY KEY,
+    task_id INT,
+    project_id INT,
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id),
+    FOREIGN KEY (project_id) REFERENCES projects(project_id)
+);
