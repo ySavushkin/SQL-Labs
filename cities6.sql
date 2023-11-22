87c085708427c2b9c3f43d3b73cfe3c17f19ae19
ySavushkin <144417407+ySavushkin@users.noreply.github.com>
2023-11-22 07:45:45 +0100

    Update cities6.sql

--- cities6.sql
+++ cities6.sql
@@ -30,15 +30,15 @@ CREATE TABLE `stations` (
   `line_id` int(10) UNSIGNED NOT NULL,
   `previous_station` varchar(255) COLLATE utf8_unicode_ci,
   `next_station` varchar(255) COLLATE utf8_unicode_ci,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`line_jd`) REFERENCES `lines`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 DROP TABLE IF EXISTS `transitions`;
 CREATE TABLE `transitions` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `start_station` int(10) unsigned NOT NULL,
   `end_station` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`start_station`, `end_station`),
   FOREIGN KEY (`start_station`) REFERENCES `stations`(`id`),
   FOREIGN KEY (`end_station`) REFERENCES `stations`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
